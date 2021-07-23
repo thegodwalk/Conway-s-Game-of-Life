735adb5a75775af5ab3779f000fc87588a22dd34
thegodwalk <ianreininger1@gmail.com>
2021-07-23 18:55:29 +1000

    Update Test.js

--- Test.js
+++ Test.js
@@ -355,7 +355,7 @@ function DrawSquare(Cell,on){
         state=0;
          
          for(let re = -1; i+re>=0 && i+re<numx && -1<=re && re<=1;re++){
-           for(let pe = -1; j+pe>=0 && j+pe<numy && -1<=pe && pe<=1 && abs(re)+abs(pe)!=0;pe++){
+           for(let pe = 0; j+pe>=0 && j+pe<numy && 0<=pe && pe<=2 && pe-re!=-1;pe++){
              
            
            state=state+
